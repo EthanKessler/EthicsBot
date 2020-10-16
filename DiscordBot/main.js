c33cdddc2b88130eb66314fceafb1dfe19e7ae65
EthanKessler <EthanKessler.dev@gmail.com>
2020-10-16 20:52:13 +0100

    Update main.js

--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -25,9 +25,6 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    //TB REMOVED
-    message.channel.send(args);
-
 //Adding all the commands
     if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
