4e7b25ffa53f4265f6753debb83c99532b92284b
EthanKessler <EthanKessler.dev@gmail.com>
2020-10-17 18:28:15 +0100

    Added finding quotes

---
+++ DiscordBot/commands/topic.js
@@ -0,0 +1,14 @@
+const ScienceTopics = []
+
+module.exports = {
+    name: 'invite',
+    description: "send invite link",
+
+    //Temp solution
+    Output: 'https://discord.gg/Z8SmX5C',
+
+    execute(message, args){
+
+        message.author.send(this.Output);
+    }
+}
