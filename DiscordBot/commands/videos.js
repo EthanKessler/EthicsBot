43704c05daed45cc75f82e958ee11d03a2f76746
EthanKessler <EthanKessler.dev@gmail.com>
2020-10-17 08:38:13 +0100

    Update videos.js

--- DiscordBot/commands/videos.js
+++ DiscordBot/commands/videos.js
@@ -1,8 +1,11 @@
+Dict = ["orange", "correct", "rememberer", "moon", "ants"],
+Def =  ["https://www.youtube.com/watch?v=WX0xWJpr0FY&t=1s", "https://www.youtube.com/watch?v=DJiGuFCzaFo&t=280s", "https://www.youtube.com/watch?v=hS_AXRRnIzM", "https://www.youtube.com/watch?v=K3X2Fv-c3Fc", "https://www.youtube.com/watch?v=Et6itTuJSYY"],
+
 
 module.exports = {
     //Setting up dictionary
-    Dict: ["orange", "correct", "rememberer", "moon", "ants"],
-    Def: ["https://www.youtube.com/watch?v=WX0xWJpr0FY&t=1s", "https://www.youtube.com/watch?v=DJiGuFCzaFo&t=280s", "https://www.youtube.com/watch?v=hS_AXRRnIzM", "https://www.youtube.com/watch?v=K3X2Fv-c3Fc", "https://www.youtube.com/watch?v=Et6itTuJSYY"],
+    // Dict: ["orange", "correct", "rememberer", "moon", "ants"],
+    // Def: ["https://www.youtube.com/watch?v=WX0xWJpr0FY&t=1s", "https://www.youtube.com/watch?v=DJiGuFCzaFo&t=280s", "https://www.youtube.com/watch?v=hS_AXRRnIzM", "https://www.youtube.com/watch?v=K3X2Fv-c3Fc", "https://www.youtube.com/watch?v=Et6itTuJSYY"],
     Index: 0,
 
 
