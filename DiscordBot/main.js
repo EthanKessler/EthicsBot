da4784f834e7979781e9071d624a43a127e81439
EthanKessler <EthanKessler.dev@gmail.com>
2020-10-18 19:52:23 +0100

    Update main.js

--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
-const prefix = '.';
+const prefix = '.t';
 
 const fs = require('fs');
 
