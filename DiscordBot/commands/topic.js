15306b4471109b313774c5d93810366272a3ad97
EthanKessler <EthanKessler.dev@gmail.com>
2020-10-20 11:17:52 +0100

    Update topic.js

--- DiscordBot/commands/topic.js
+++ DiscordBot/commands/topic.js
@@ -5,6 +5,7 @@ const ScienceTopics = ["Ageing can be slowed down and reversed.", "Socio-economi
                         "Is articficial intelligence a threat to humans?", "Is technology helping people or making them lazy?", "Do advancements in technology mean a decrease in the growth of nature?",
                         "Should internet use need censorship or control?", "Is colonization of Mars a good idea?", "Can artificial intelligence replace human intelligence?",
                         "Will humans branch out to the cosmos?", "Can climate change be reversed?", "Is genetic engineering dangerous in the long run?", "Do aliens exist?"];
+var UsedScience = [];
 
 const PhilosophyTopics = ["What is the meaning of a good life?", "Have we become less happy in this age of technology?", "Are humans obligated to better themselves and will that make them happier?",
                         "Does life require a purpose and a goal?", "What is happiness?", "Is love simply physical desire or something more?", "Does evil come from within, and if so why?",
@@ -16,26 +17,31 @@ const PhilosophyTopics = ["What is the meaning of a good life?", "Have we become
                         "Is it possible time is being altered right now?", "Does awareness of consciousness have benefits?", "Is limiting immigration to developed countries right?",
                         "Why can’t every person be a genius?", "How can we be sure there is or is not life after death?", "If death is inevitable, why bother doing anything?",
                         "Is there such a thing as a good death?", "How do you properly say goodbye to someone that has died?"];
+var UsedPhilosophy = [];
 
 const TheologyTopics = ["Does god exist?", "Is it moral to teach religion in schools?", "Did a greater being create the universe?", "Is creation 'ex nihilo' possible?", "Is there an afterlife?",
                         "Do humans exercise free will?", "Does determinism excuse us from moral obligation?", "Does the concept of reincarnation (Buddhism) make sense to you?",
                         "It is 'perfectly consistent' that god should have made the laws of physics (paraphrased, Isaac Newton)", "SHOULD god influence our daily lives? Or even the course of history?",
                         "Deism vs Theism?", "Does 'Origional sin' exist?"];
+var UsedTheology = [];
 
 const PoliticalTopics = ["Gun control", "Abortion", "Religios freedom", "Freedom of speech", "Animal rights", "Suspension of human rights in the interest of public order",
                         "Vaccines", "Rights to privacy", "Global Climate Change", "Capital punishment", "Immigration reform", "The trump presidency", "Discrimination in politics", "Positive discrimination (Does it work?)",
                         "Cancel culture", "The Israeli-Palestinian Conflict", "Universal Healthcare", "Political Censorship", "Redaction of government documents and witholding of information from general public",
                         "Social influencers using charity for leverage", "Universal ban on smoking", "Drug laws", "What steps should governments take to improve mental health?", ""];
+var UsedPolitics = [];
 
 const HistoricalTopics = ["In your opinion, what was the most significant event in history?", "To what extent was the second world war caused by the punishments enforced after WWI.", "The scramble for africa and its effects on WW1", "Who started the scramble for Africa and why?",
                         "What was the most important event of WW1 and WW2? Why?", "What would have happened had the USA elected not to use the Nuclear bomb", "To what extent did the cracking of the enigma code aid the Allies in WWII?", "Was the Roman empire always destined to fall? Why?",
                         "How did the small focus on cavalry in the ranks of the army affect the late roman Empire?", "Did the Roman's treat its occupied citizens fairly?", "Would China be better under nationalist rule?", "The significance of the 'People's volunteer army' in the Korea Crisis",
                         "In the modern day, do we have as much to learn from history as we once did?", "Does history truly repeat itself?"];
+var UsedHistory = [];
 
 const GeneralTopics = ["How's your day going?", "What was the best thing about today?", "What book are you reading?", "What did you eat for breakfast?", "What's your favourite film?",
                         "What do you do to get rid of stress?", "What three words best describe you?", "What’s your favorite way to waste time?", "Do you have any pets? What are their names?",
                         "When was the last time you worked incredibly hard?", "If you opened a business, what kind of business would it be?", "Are you a very organized person?", "What do you do to improve your mood when you are in a bad mood?",
                         "What are some things you want to accomplish before you die?", "What are you best at?", "What weird or useless talent do you have?", "What job do you do?", "What video games do you play?"];
+var UsedGeneral = [];
 
 module.exports = {
     name: 'topic',
