d83f9d40f6708fa32b38613370c2656ee3c11d52
Ash Kessler <seventy2.70two@gmail.com>
2020-11-17 08:52:25 -0800

    created a suggest.js file

---
+++ DiscordBot/commands/suggest.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'suggest',
+  description: "Suggest a topic for the discussion channels!",
+
+  execute(message){
+
+    // incomplete
+    }
+}
--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -12,7 +12,7 @@ const subs = fs.readdirSync('./subs/').filter(file => file.endsWith('.vtt'));
 const CoolDown = [];
 var AllowAccess = true;
 var AllowRandomPosts = false;
-const Whitelist = ['776852904983396412', '208428556106072064', '256131859950338054'];
+const Whitelist = ['776852904983396412', '208428556106072064', '256131859950338054', '689966022534627398'];
 const Owner = '776852904983396412'
 var CommandsAnswered = 0;
 var Waittime = (Math.random() * (14400000 - 3600000));
