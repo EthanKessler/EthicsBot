48d7bebd0ed39e853dc922c77427c85a2b85c6a6
Ash Kessler <seventy2.70two@gmail.com>
2020-11-17 11:53:23 -0800

    suggest.js (pls work ;-;)

--- DiscordBot/commands/suggest.js
+++ DiscordBot/commands/suggest.js
@@ -2,8 +2,22 @@ module.exports = {
   name: 'suggest',
   description: "Suggest a topic for the discussion channels!",
 
-  execute(message){
+  execute(message, args, client) {
 
-    // incomplete
+    var suggestion = args.join(" ");
+
+    if (!suggestion) {
+      return message.channel;
+        message.reply("you didn't pose a question!");
+        .then(message => {
+          message.delete(10000) }); }
+
+    if (suggestion) {
+      message.delete();
+      const user = client.users.cache.get('689966022534627398');
+      message.user.send("**" + message.author.username() + "** made a suggestion:\n" + suggestion);
+      setTimeout() => { message.channel.reply("your suggestion was sent to the developers!"); }, 10000);
     }
+
+  }
 }
