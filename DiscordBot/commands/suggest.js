47956f80414b1b8aa1b0362ff3e6f6b3ba308312
EthanKessler <EthanKessler.dev@gmail.com>
2020-11-19 19:09:23 +0000

    fixing

--- DiscordBot/commands/suggest.js
+++ DiscordBot/commands/suggest.js
@@ -1,23 +1,23 @@
-module.exports = {
-  name: 'suggest',
-  description: "Suggest a topic for the discussion channels!",
+// module.exports = {
+//   name: 'suggest',
+//   description: "Suggest a topic for the discussion channels!",
 
-  execute(message, args, client) {
+//   execute(message, args, client) {
 
-    var suggestion = args.join(" ");
+//     var suggestion = args.join(" ");
 
-    if (!suggestion) {
-      return message.channel;
-        message.reply("you didn't pose a question!");
-        .then(message => {
-          message.delete(10000) }); }
+//     if (!suggestion) {
+//       return message.channel;
+//         message.reply("you didn't pose a question!");
+//         .then(message => {
+//           message.delete(10000) }); }
 
-    if (suggestion) {
-      message.delete();
-      const user = client.users.cache.get('689966022534627398');
-      message.user.send("**" + message.author.username() + "** made a suggestion:\n" + suggestion);
-      setTimeout() => { message.channel.reply("your suggestion was sent to the developers!"); }, 10000);
-    }
+//     if (suggestion) {
+//       message.delete();
+//       const user = client.users.cache.get('689966022534627398');
+//       message.user.send("**" + message.author.username() + "** made a suggestion:\n" + suggestion);
+//       setTimeout() => { message.channel.reply("your suggestion was sent to the developers!"); }, 10000);
+//     }
 
-  }
-}
+//   }
+// }
