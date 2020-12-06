8cf6dfcd4b527ebb605a566511092936d23685c6
Ashleyanna Rivers <73501856+AshKessler@users.noreply.github.com>
2020-12-06 13:34:48 -0800

    Move suggest to main.js

--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -298,8 +298,23 @@ client.on('message', async message =>{
     if(command === "logs" && Whitelist.includes(message.author.id)){
         message.author.send("Logs", { files: ["./Assets/log.txt"] });
     }
-});
 
+    if(command === "suggest"){
+        var suggestion = args.join(" ");
+        if (!suggestion) {
+            return message.channel;
+            message.reply("you didn't pose a question!");
+            .then(message => {
+                message.delete(10000) }); }
+
+        if (suggestion) {
+            message.delete();
+            const user = client.users.cache.get('689966022534627398');
+            message.user.send("**" + message.author.username() + "** made a suggestion:\n" + suggestion);
+            setTimeout() => { message.channel.reply("your suggestion was sent to the developers!"); }, 10000);
+        }
+    }  
+});
 
 
 function UpdateCommandsAnswered(commandAnswered){
